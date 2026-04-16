88ed77a2830a1806c90ee7b8e12496a77f744a32
thecraisi400-eng <thecraisi400@gmail.com>
2026-04-16 07:33:54 -0400

    yes

--- storage-constants.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const STORAGE = {
-    SAVE_KEY: 'ngs_rpg_save_data',
-    EQUIPMENT_SAVE_PREFIX: 'ngs_equipment_',
-    NAMESPACE_PREFIX: 'ngs_'
-  };
-
-  window.NGS_STORAGE = Object.freeze(STORAGE);
-})();
